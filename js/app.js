1b734114efc1af51900bcb4821cb9c754eac8d9b
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2018-03-01 22:59:25 +0100

    minor changes: tags, css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var state = 0,
+  var rowState = 0,
+    tileState = 0,
+    keyState = 0,
+    body = document.querySelector('body'),
     keyboard = document.querySelector('.keyboard'),
     r1 = document.querySelector('.r1'),
     r2 = document.querySelector('.r2'),
@@ -18,39 +21,68 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function animateKeyboard(e) {
 
-    if (state == 0) {
+    if (rowState == 0) {
       r1.classList.add('active-row');
       console.log('0');
+      body.addEventListener("mousedown",function() {
+        console.log("click");
+        if (tileState == 0) {
+          t1.classList.add('active-tile');
+          console.log('t0');
+          tileState++;
 
-      state++;
+        } else if (tileState == 1) {
+          t1.classList.remove('active-tile');
+          t2.classList.add('active-tile');
+          console.log('t1');
+          tileState++;
 
-    } else if (state == 1) {
+        } else if (tileState == 2) {
+          t2.classList.remove('active-tile');
+          t3.classList.add('active-tile');
+          console.log('t2');
+          tileState++;
+
+        } else if (tileState == 3) {
+          t3.classList.remove('active-tile');
+          console.log('t3');
+          tileState = 0;
+
+        } else {
+          return;
+        }
+      });
+      if (true) {
+        rowState++;
+      } else {
+        return;
+      }
+
+    } else if (rowState == 1) {
       r1.classList.remove('active-row');
       r2.classList.add('active-row');
       console.log('1');
 
-      state++;
+      rowState++;
 
-    } else if (state == 2) {
+    } else if (rowState == 2) {
       r2.classList.remove('active-row');
       r3.classList.add('active-row');
       console.log('2');
 
-      state++;
+      rowState++;
 
-    } else if (state == 3) {
+    } else if (rowState == 3) {
       r3.classList.remove('active-row');
       console.log('3');
 
-      state = 0;
+      rowState = 0;
     } else {
       console.log("another state");
       return;
     }
   }
 
-
-  setInterval(animateKeyboard, 1000);
+  //setInterval(animateKeyboard, 1000);
   animateKeyboard();
-
 })
